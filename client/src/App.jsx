50d531a30adc08a2909bf3c463a9baf9ce216de2
anuj112003 <140871738+anuj112003@users.noreply.github.com>
2024-04-10 19:21:43 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
     const handleGetUser = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/user/me",
+          "https://task-manager-1-k0qr.onrender.com/api/v1/user/me",
           { withCredentials: true }
         );
         setIsAuthenticated(true);
